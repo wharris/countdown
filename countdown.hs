fc9c4888c71fabb0c969719e839500de9a6c786b
Will Harris <will@greatlibrary.net>
2012-09-07 15:27:42 +0100

    Get games from command line.

--- countdown.hs
+++ countdown.hs
@@ -1,8 +1,19 @@
 import Data.Maybe
 import Data.List
 import Game
+import System.Environment ( getArgs )
 
-game = Game [25,50,75,100,3,6] 952
-game2 = Game [75, 50, 2, 3, 8, 7] 812
+-- nice games:
+-- 25 50 75 100 3 6 = 952
+-- 75 80 2 3 8 7 = 812
 
-main = putStrLn . show . bestAnswer $ game
+parseArgs :: [String] -> Game
+parseArgs args 
+    | length args == 7 = Game numbers target
+    | otherwise        = error "usage: number*6 target"
+    where numbers = map read (init args)
+          target = read (last args)
+
+main = do
+    args <- getArgs
+    putStrLn . show . bestAnswer . parseArgs $ args
